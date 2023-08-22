d5f38a4237d107ae77c0618271f7c58cfd4cf7f8
Karolis <karolis@localhost.localdomain>
2023-08-22 18:19:05 +0300

    Desktop design

---
+++ product-preview-card-component-main/tailwind.config.js
@@ -0,0 +1,13 @@
+//Include purely in order for tailwindcss intellisense plugin to work
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary1: "hsl(158, 36%, 37%)",
+        primary2: "hsl(30, 38%, 92%)",
+        neutral1: "hsl(212, 21%, 14%)",
+        neutral2: "hsl(228, 12%, 48%)",
+      },
+    },
+  },
+};
