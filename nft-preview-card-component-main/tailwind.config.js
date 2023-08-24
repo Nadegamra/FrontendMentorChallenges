1dc1b1a81c7a68b905d0b7a376c517d874148b69
Karolis <karolis@localhost.localdomain>
2023-08-24 21:39:31 +0300

    Finished nft preview card

---
+++ nft-preview-card-component-main/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        softBlue: "hsl(215, 51%, 70%)",
+        cyan: "hsl(178, 100%, 50%)",
+        mainBg: "hsl(217, 54%, 11%)",
+        cardBg: "hsl(216, 50%, 16%)",
+        textColor: "hsl(215, 32%, 60%)",
+        lineColor: "hsl(215, 32%, 27%)",
+      },
+    },
+  },
+};
