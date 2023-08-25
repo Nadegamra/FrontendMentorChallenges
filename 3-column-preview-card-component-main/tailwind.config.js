10a200240d7e7b18d7920e746eeafbec2fc2cf9d
Karolis <karolis@localhost.localdomain>
2023-08-25 16:14:00 +0300

    Finished 3-column-preview-card

---
+++ 3-column-preview-card-component-main/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  theme: {
+    extend: {
+      screens: {
+        desktop: "1100px",
+      },
+      colors: {
+        brightOrange: "hsl(31, 77%, 52%)",
+        darkCyan: "hsl(184, 100%, 22%)",
+        veryDarkCyan: "hsl(179, 100%, 13%)",
+        paragraphColor: "hsla(0, 0%, 100%, 0.75)",
+        veryLightGray: "hsl(0, 0%, 95%)",
+      },
+    },
+  },
+};
