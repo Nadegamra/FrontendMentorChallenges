6d67876fbb16bf950515fa8f7f1c823afef66398
Karolis <karolis@localhost.localdomain>
2023-08-29 13:09:17 +0300

    Mostly finished

---
+++ social-proof-section-master/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  theme: {
+    screens: {
+      desktop: "1350px",
+    },
+    extend: {
+      colors: {
+        veryDarkMagenta: "hsl(300, 43%, 22%)",
+        softPink: "hsl(333, 80%, 67%)",
+        darkGrayishMagenta: "hsl(303, 10%, 53%)",
+        lightGrayishMagenta: "hsl(300, 24%, 96%)",
+      },
+    },
+  },
+};
