cc166ca23043be566f79b42cc7ac42ee94ba4fa6
Karolis <karolis@localhost.localdomain>
2023-08-29 23:30:41 +0300

    Desktop bg pattern

--- social-proof-section-master/tailwind.config.js
+++ social-proof-section-master/tailwind.config.js
@@ -4,6 +4,10 @@ export default {
       desktop: "1350px",
     },
     extend: {
+      content: {
+        bgBottomDesktop: 'url("images/bg-pattern-bottom-desktop.svg")',
+        bgTopDesktop: 'url("images/bg-pattern-top-desktop.svg")',
+      },
       colors: {
         veryDarkMagenta: "hsl(300, 43%, 22%)",
         softPink: "hsl(333, 80%, 67%)",
